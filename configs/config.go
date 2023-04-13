729322d97a11056e36b327f11c5208b63b4c885e
Vladislav Ramus <vgramus@mts.ru>
2023-04-13 11:45:45 +0300

    fixed cfg handling

--- configs/config.go
+++ configs/config.go
@@ -13,33 +13,77 @@ type Config struct {
 }
 
 type App struct {
-	Name                 string `env-required:"true" yaml:"name"`
-	Version              string `env-required:"true" yaml:"version"`
-	AccrualSystemAddress string `env:"ACCRUAL_SYSTEM_ADDRESS"`
+	Name                 string `env-required:"true" env-default:"gophemart"`
+	Version              string `default:"0.0.1"`
+	AccrualSystemAddress string `env:"ACCRUAL_SYSTEM_ADDRESS" env-required:"true"`
 }
 
 type Client struct {
-	Address string `env-required:"true" yaml:"address" env:"RUN_ADDRESS"`
+	Address string `env-required:"true" env:"RUN_ADDRESS"`
 }
 
 type Logger struct {
-	Level string `yaml:"level"`
+	Level string `yaml:"level" yaml-default:"debug"`
 }
 
 type Database struct {
-	URI string `env:"DATABASE_URI"`
+	URI string `env:"DATABASE_URI" env-required:"true"`
 }
 
-func NewConfig() *Config {
-	cfg := &Config{}
-
+func (cfg *Config) parseFlags() {
 	flag.StringVar(&cfg.Client.Address, "a", cfg.Client.Address, "address to listen on")
 	flag.StringVar(&cfg.Database.URI, "d", cfg.Database.URI, "database URI")
 	flag.StringVar(&cfg.App.AccrualSystemAddress, "r", cfg.App.AccrualSystemAddress, "accrual address")
 
-	_ = cleanenv.ReadConfig("./config/config.yaml", cfg)
+	flag.Parse()
+}
 
-	_ = cleanenv.ReadEnv(cfg)
+func (cfg *Config) updateConfig(v *Config) {
+	if v.AccrualSystemAddress != "" && v.AccrualSystemAddress != cfg.AccrualSystemAddress {
+		cfg.AccrualSystemAddress = v.AccrualSystemAddress
+	}
+
+	if v.Database.URI != "" && v.Database.URI != cfg.Database.URI {
+		cfg.Database.URI = v.Database.URI
+	}
+
+	if v.Address != "" && v.Address != cfg.Address {
+		cfg.Address = v.Address
+	}
+}
+
+const (
+	appName            = "gophemart"
+	appVersion         = "0.0.1"
+	loggerDefaultLevel = "debug"
+)
+
+func defaultConfig() *Config {
+	return &Config{
+		App: App{
+			Name:    appName,
+			Version: appVersion,
+		},
+		Logger: Logger{Level: loggerDefaultLevel},
+	}
+}
+
+func NewConfig() *Config {
+	var (
+		cfg   *Config
+		envs  *Config
+		flags *Config
+	)
+
+	cfg = defaultConfig()
+
+	flags = new(Config)
+	flags.parseFlags()
+	cfg.updateConfig(flags)
+
+	envs = new(Config)
+	_ = cleanenv.ReadEnv(envs)
+	cfg.updateConfig(envs)
 
 	return cfg
 }
